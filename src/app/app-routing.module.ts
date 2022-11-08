85290116f5c2ead3834f236ba2c67514769e01ad
YamilLazzariDicsys <yamil.lazzari@dicsys.com>
2022-11-08 10:12:22 -0300

    Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { TaskComponent } from './task/task.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'task',
     loadChildren:() => import('./task/task.module').then(m=>m.TaskModule)
