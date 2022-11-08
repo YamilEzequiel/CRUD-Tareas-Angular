410c802a92b39e68867459834e0a5f9cec6e9586
YamilLazzariDicsys <yamil.lazzari@dicsys.com>
2022-11-08 09:23:23 -0300

    Generate Modulo/Routing/Component task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskComponent } from './task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'task',
+    loadChildren:() => import('./task/task.module').then(m=>m.TaskModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
